329544bd65f6718872ea815d23ec901314a48b70
conorprunty <conorprunty@hotmail.com>
2016-11-29 19:02:38 +0000

    initial set up

---
+++ src/main/java/com/groupa/banksystem/Customer.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.groupa.banksystem;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author conorprunty
+ */
+@Entity
+@Table(name = "customer")
+@XmlRootElement
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String address;
+
+    private String email;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+    public Customer() {
+    }
+
+}
---
+++ src/main/java/com/groupa/banksystem/CustomerService.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.groupa.banksystem;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author conorprunty
+ */
+@Path("/customers")
+public class CustomerService {
+    
+    EntityManager entityManager;
+    
+    public CustomerService(){
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Customer");
+        entityManager = emfactory.createEntityManager();
+    }
+
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Customer> getPlanets() {
+       return allEntries();
+    }
+    
+     public List<Customer> allEntries() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Customer> cq = cb.createQuery(Customer.class);
+        Root<Customer> rootEntry = cq.from(Customer.class);
+        CriteriaQuery<Customer> all = cq.select(rootEntry);
+        TypedQuery<Customer> allQuery = entityManager.createQuery(all);
+        return allQuery.getResultList();
+    }
+     
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Path("{id}")
+    public Customer getPlanet(@PathParam("id") int id) {
+        Customer test = entityManager.find(Customer.class, id);
+        return test;
+    }
+    
+    @POST
+    @Path("/save")
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response save(Customer c) {
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(c);
+        entityManager.getTransaction().commit();
+        
+        entityManager.close();
+        entityManager.close();
+
+        return Response.status(200).entity(c).build();
+    }
+}
